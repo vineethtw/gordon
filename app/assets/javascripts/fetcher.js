703d4542ad6a84b2880d8c52283da696e59542ef
Vineeth Venudasan <vineeth.venu@gmail.com>
2014-11-16 01:15:27 +0530

    Adding more functionality
    Twitter objects in JS, and the controller now returns
    all info related to a tweet.

--- app/assets/javascripts/fetcher.js
+++ app/assets/javascripts/fetcher.js
@@ -1,5 +1,42 @@
+function Tweet(message, profile_pic, media_url){
+    var self = this;
+    self.message = message;
+    self.profile_pic = profile_pic;
+    self.media_url = media_url;
+}
+
+function convert_to_objects(tweet_data){
+    var tweets_on_screen = [];
+    $.each($.parseJSON(tweet_data), function(index, tweet){
+        tweets_on_screen.push(new Tweet(tweet["message"],tweet["profile_image_uri"],tweet["media_uris"]));
+    });
+    return tweets_on_screen;
+}
+
+function TweetDisplayObject(tweet){
+    this.as_html = function(){
+        var html = String.format(
+            '<div>' +
+                '<span>' +
+                    '<img id="profile_pic" src="{0}"/>' +
+                '</span>' +
+                '<span>' +
+                    '<div id="tweet_message">{1}</div>' +
+                '</span>' +
+            '</div>', tweet.profile_pic, tweet.message);
+        $(html).appendTo($('#tweet_objects'));
+    }
+}
+
 var eventSource = new EventSource("http://localhost:3000/tweets/search");
 
 eventSource.onmessage = function(event) {
     $("#tweet_container").html(event.data);
+
+    var tweets = convert_to_objects(event.data);
+    new TweetDisplayObject(tweets[0]).as_html();
+
+
+
+
 };
--- app/models/tweets.rb
+++ app/models/tweets.rb
@@ -2,8 +2,11 @@
     def recent_tweets(hashtag, number_of_results=100)
       tweets = []
       $twitter.search(hashtag, result_type: "recent").take(number_of_results).each do |tweet|
+
         tweets << {
-            'message' => tweet.full_text
+            'message' => tweet.full_text,
+            'profile_image_uri' => tweet.user.profile_image_uri(size=:original).to_s,
+            'media_uris'=> tweet.media? ? tweet.media.collect {|m| m.media_uri.to_s if m.is_a?(Twitter::Media::Photo)}: []
         }
       end
       tweets

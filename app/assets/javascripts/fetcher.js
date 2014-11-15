c83e31c7222008eb426ba6be72020e3939f0b6fe
Vineeth Venudasan <vineeth.venu@gmail.com>
2014-11-16 02:23:01 +0530

    Display profile pics on screen, and show messages..
    as tooltips. Used jBox for this.

--- app/assets/javascripts/fetcher.js
+++ app/assets/javascripts/fetcher.js
@@ -1,5 +1,6 @@
-function Tweet(message, profile_pic, media_url){
+function Tweet(id, message, profile_pic, media_url){
     var self = this;
+    self.id = id;
     self.message = message;
     self.profile_pic = profile_pic;
     self.media_url = media_url;
@@ -8,34 +9,35 @@ function Tweet(message, profile_pic, media_url){
 function convert_to_objects(tweet_data){
     var tweets_on_screen = [];
     $.each($.parseJSON(tweet_data), function(index, tweet){
-        tweets_on_screen.push(new Tweet(tweet["message"],tweet["profile_image_uri"],tweet["media_uris"]));
+        tweets_on_screen.push(new Tweet(tweet["id"], tweet["message"], tweet["profile_image_uri"], tweet["media_uris"]));
     });
     return tweets_on_screen;
 }
 
 function TweetDisplayObject(tweet){
     this.as_html = function(){
-        var html = String.format(
-            '<div>' +
-                '<span>' +
-                    '<img id="profile_pic" src="{0}"/>' +
-                '</span>' +
-                '<span>' +
-                    '<div id="tweet_message">{1}</div>' +
-                '</span>' +
-            '</div>', tweet.profile_pic, tweet.message);
-        $(html).appendTo($('#tweet_objects'));
+        var image = String.format('<img class="profile_pic" id="profile_pic_{0}" src="{1}"/>', tweet.id, tweet.profile_pic);
+        var tooltip = String.format('<div style="display: none" class="tooltip_popup" id="{0}" >{1}</div>', tweet.id, tweet.message);
+        $(image).appendTo($('#tweet_objects'));
+        $(tooltip).appendTo($('#tweet_objects'));
+        $(String.format("#profile_pic_{0}", tweet.id)).jBox('Tooltip',{
+            content: $(String.format('#{0}', tweet.id)),
+            animation: 'flip'
+        });
     }
 }
 
 var eventSource = new EventSource("http://localhost:3000/tweets/search");
 
 eventSource.onmessage = function(event) {
-    $("#tweet_container").html(event.data);
-
+    //$("#tweet_container").html(event.data);
+    $('#tweet_objects').empty();
     var tweets = convert_to_objects(event.data);
-    new TweetDisplayObject(tweets[0]).as_html();
+    $.each(tweets, function(index, tweet){
+        new TweetDisplayObject(tweet).as_html();
+    });
 
+    eventSource.close();
 
 
 
--- app/models/tweets.rb
+++ app/models/tweets.rb
@@ -4,6 +4,7 @@
       $twitter.search(hashtag, result_type: "recent").take(number_of_results).each do |tweet|
 
         tweets << {
+            'id' => tweet.id,
             'message' => tweet.full_text,
             'profile_image_uri' => tweet.user.profile_image_uri(size=:original).to_s,
             'media_uris'=> tweet.media? ? tweet.media.collect {|m| m.media_uri.to_s if m.is_a?(Twitter::Media::Photo)}: []
